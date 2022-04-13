1b6c3893212dc3b952e0089ec9b36c439cb7b07d
ssufunny <wltnwhgdkgo@naver.com>
2022-04-14 03:33:01 +0900

    Update mecab file "코드 정리 및 주석 추가"

--- mecab.py
+++ mecab.py
@@ -21,14 +21,14 @@ print("MeCab 이용")
 texts = m.parse(text_file)
 # 띄어쓰기, 줄바꿈마다 split
 words = re.split('[\t\n]', texts)
-# 생성된 배열 길이
-words_len = len(words)  # 8328
 
 tagging = []
 word = []
+plz = []
+result = []
 
 # 단어와 품사 태깅 부분으로 배열 따로 지정
-for i in range(0, words_len - 1):   # 배열 길이만큼 반복
+for i in range(0, len(words) - 1):   # 배열 길이만큼 반복
     if i % 2 == 1:  # 품사 태깅
         tagging.append(words[i])
     else:   # 단어
@@ -39,64 +39,18 @@ for i in range(0, words_len - 1):   # 배열 길이만큼 반복
 tagging_join = ",".join(tagging)
 # , 마다 split
 tagging_split = re.split('[,]', tagging_join)
-# 리스트 -> 배열
-tagging_split_arr = np.array(tagging_split)
-text = word + tagging_split
-text_arr = np.array(text)
-plz = []
 
-for number in range(0, len(tagging_split_arr) - 1):
+# 지명 정보 부분만 추출
+for number in range(0, len(tagging_split) - 1):
     if number % 8 == 1:
         plz.append(tagging_split[number])
-plz_arr = np.array(plz)
-word_arr = np.array(word)
-# for ss in range(0, len(word) - 1):
-#     print(ss, word_arr[ss], plz_arr[ss])
-#     # print(number)
-
-print(len(tagging_split))
-word_arr = np.array(word)
-
-result = []
+# 결과 배열
 result = [[str(1000)]*2 for i in range(len(tagging))]
-result_arr = np.array(result)
 
+# 결과 배열에 값 넣어주기
 for n in range(0, len(word) - 1):
-    result_arr[n][0] = word_arr[n]
-    result_arr[n][1] = plz_arr[n]
-for sss in range(0, len(word) - 1):
-    print(result_arr[sss])
-
-
-#
-# print(text)
-# # print(tagging)
-print(len(tagging)) # 4163
-# print(len(word))    # 4163
-# print(len(tagging_split))   # 33304
-# print(len(text))    # 37467
-# word_tagging = []
-# print(word_split)
-# print(tagging_split)
-# text =
-#
-# for i in texts:
-#     for j in range(7):
-#         texts[i][j] =
-# print(texts[0])
-
-
-# # Mecab 이용
-# print("Mecab 이용")
-# # pos 함수 사용(형태소 분석 & 품사 매칭)
-# print(mecab.pos(text_file))
-
-
-# # 명사 추출
-# noun = mecab.nouns(text)
-# count = Counter(noun)
-#
-# # 가장 많은 명사 100개 출력
-# noun_list = count.most_common(100)
-# for v in noun_list:
-#     print(v)
+    result[n][0] = word[n]
+    result[n][1] = plz[n]
+# 단어별 출력
+for m in range(0, len(word) - 1):
+    print(result[m])
