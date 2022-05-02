adcc9d8046b16ac5baf5aa0c4bb5167e23ae7804
Ryuhyisu <skyblue22756@gmail.com>
2022-05-02 11:51:07 +0900

    05.02 flask 서버 파일 (노트북에서 하랴궁..)

--- server.py
+++ server.py
@@ -1,9 +1,20 @@
 from flask import Flask
+import random
 app = Flask(__name__)
 
+[{'id':1, 'title': html}]
+#기본페이지
 @app.route('/')
 def index():
-    return 'hi'
+    return '''<!doctype html>
+    <html>
+        <body>
+            <h1><a = href="/">WEB</a></h1>
+            <ol>
+
+@app.route('/create/')
+def create():
+    return 'Create'
 
 # 개발하는거 웹페이지에 바로바로 반영되도록 ( 배포할때는 debug=True 없애주기 )
 app.run(port=5001, debug=True)
