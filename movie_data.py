68bb8ae9b7a022e1c49a94521d3c686de49ecce6
ssufunny <wltnwhgdkgo@naver.com>
2022-05-05 19:04:11 +0900

    Update movie_data.py file "KOBIS 박스오피스 데이터 이용해서 영화 제목 추출하기"

--- movie_data.py
+++ movie_data.py
@@ -1,18 +1,23 @@
 import pandas as pd
-import requests
-import json
+import openpyxl
 
-url = 'http://kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=5204b505ee5c0d2aa5ffa16314078b47&repNationCd=22041011&'
+filename = 'C://Users//jisu//Documents//한국외대//2022 - 1//정보통신종합설계//Data//movie_list.xlsx'
+with open("C:/mecab/user-dic/nnp.csv", 'r', encoding='utf-8') as f:
+    file_data = f.readlines()
+file_data
 
-res = requests.get(url)
-text = res.text
+movie_list = pd.read_excel(filename, engine="openpyxl")
 
-d = json.loads(text)
-
-movie_list = []
-
-for b in d['movieListResult']['movieList']:
-    movie_list.append(b['movieNm'])
-
-data = pd.DataFrame(movie_list)
-data.to_csv("movie_list.txt", mode='w', encoding='utf-8', index=False)
+for i in range(len(movie_list)):
+    movie_name = movie_list.iloc[i, 1]
+    # 받침 유무 판단
+    if (ord(movie_name[-1]) - 44032) == 0:  # 받침 없음
+        TF = 'F'
+    else:
+        TF = 'T'
+    print(movie_name, TF)
+    # mecab 사용자 사전에 추가
+    # file_data.append(movie_name + ',,,,NNP,미디어명,' + TF + ',' + movie_name + ',*,*,*,*\n')
+    # with open("C:/mecab/user-dic/nnp.csv", 'w', encoding='utf-8') as f:
+    #     for line in file_data:
+    #         f.write(line)
